f35ecdbb8d6f9776b1836ce8bd24fdb54ecc2be7
leshka0 <robin.leshka@gmail.com>
2018-01-09 00:51:44 +0000

    + nike

--- js/intro.js
+++ js/intro.js
@@ -1,12 +1,16 @@
 function intro() {
   $(".toThat").on( "click", function() {
     console.log("this");
+    $(".toThis").html("normal shits")
+    $(".toThat").html("crazy shits")
     $(".this").addClass("switchThis");
     $(".that").addClass("switchThat");
     $(".intro").addClass("thatIntro");
   });
   $(".toThis").on( "click", function() {
     console.log("that");
+    $(".toThis").html("unique identities")
+    $(".toThat").html("immersive experiences")
     $(".this").removeClass("switchThis");
     $(".that").removeClass("switchThat");
     $(".intro").removeClass("thatIntro");
---
+++ projects/nike-infinite/nike-infinite.php
@@ -0,0 +1,42 @@
+
+<!-- Nike Infinite -->
+
+<div class="project">
+  <div class="heading">N</div>
+  <h2>Nike Infinite</h2>
+  <p class="subtitle">Art Direction, Design</p>
+  <p class="subtitle"> <a href="http://www.random.nu/nike-free/">view the case study</a> </p>
+  <div class="wrapper">
+      <div class="youtube" data-embed="1uFOEcmGbIg">
+          <div class="play-button"></div>
+      </div>
+  </div>
+
+  <div class="wrapper article">
+    <div class="big-left">
+      <div class="sequence full" speed="15" timeline="false" autoplay=true >
+        <img class="full" src="projects/nike-infinite/img/2.jpg" />
+        <img class="full" src="projects/nike-infinite/img/1.jpg" />
+        <img class="full" src="projects/nike-infinite/img/5.jpg" />
+      </div>
+    </div>
+    <h4 class="lil-right">For the launch of the next generation Nike Free outsole, we developed an interactive installation to capture the Auxetic Sole Technology : a structure that spontaneously expands and contracts upon impact. We created a range of hands-on digital and physical tools to enable the Nike teams to interact in a tactile way with the technology.</h4>
+    <div class="lil-right">
+      <img class="lazyload full" src="projects/nike-infinite/img/4_thumb.jpg" data-src="projects/nike-infinite/img/4.jpg" />
+    </div>
+  </div>
+  <div class="separator"></div>
+  <div class="wrapper article">
+    <div class="big-right">
+      <div class="sequence full" speed="15" timeline="false" autoplay=true >
+        <img class="full" src="projects/nike-infinite/img/7.jpg" />
+        <img class="full" src="projects/nike-infinite/img/8.jpg" />
+        <img class="full" src="projects/nike-infinite/img/9.jpg" />
+        <img class="full" src="projects/nike-infinite/img/11.jpg" />
+      </div>
+      <p>We gave special attention to grant a design quality to the fixtures housing the projector and the flexible fabric.</p>
+    </div>
+    <p class="lil-left">Reacting to the user’s touch, the interactive, flexible screen of the installation challenges the boundaries between physical and digital while capturing the adjustable character of the new sole. The interactive mesh was developed through a combination of visual programming, interactive sound, backlit projection and depth tracking. A cross platform concept, the installation was translated into three different applications. The projection on digital fabric lived first in London, Berlin, and Madrid during training sessions for Nike product specialists. The same visual treatment was deployed to Kinect-connected retail installations in Nike Town London. Finally, a didactic iPad interface, was spread to 40 product specialists across Europe, in order to train retail teams. All design of software and hardware, UX, and fixture product design (built in close collaboration with Thein & Rios) were developed in-house, in a time frame of only six weeks.</p>
+  </div>
+<div class="separator"></div>
+</div>
