2df98f2a661bd3d05456d45e651b93037049bc2f
leshka0 <robin.leshka@gmail.com>
2018-02-08 19:51:23 +0000

    my most beautiful nightmare

--- projects/my_most_beautiful_nightmare/mymostbeautifulnightmare.php
+++ projects/my_most_beautiful_nightmare/mymostbeautifulnightmare.php
@@ -7,7 +7,7 @@
   <p class="subtitle">Art Direction, Design</p>
   <!-- <p class="subtitle"> <a href="http://www.random.nu/nike-free/">view the case study</a> </p> -->
   <div class="wrapper">
-      <img class="full" src="projects/my_most_beautiful_nightmare/img/banner.jpg" />
+      <img class="full" src="projects/my_most_beautiful_nightmare/img/intro.jpg" />
   </div>
   <div class="wrapper">
     <h4>My most beautiful nightmare is a digital project that invites you to rediscover that dream that until today you simply can't forget.<br/><br/>
@@ -20,7 +20,8 @@ Trough this piece of digital poetry, I want to sensibilise people about the beau
         <source src="projects/my_most_beautiful_nightmare/img/01.mp4" type="video/mp4">
       </video>
       <div class="separator"></div>
-      <p>Reacting to the user’s touch, the interactive, flexible screen of the installation challenges the boundaries between physical and digital while capturing the adjustable character of the new sole. The interactive mesh was developed through a combination of visual programming, interactive sound, backlit projection and depth tracking. A cross platform concept, the installation was translated into three different applications. The projection on digital fabric lived first in London, Berlin, and Madrid during training sessions for Nike product specialists. The same visual treatment was deployed to Kinect-connected retail installations in Nike Town London. Finally, a didactic iPad interface, was spread to 40 product specialists across Europe, in order to train retail teams. All design of software and hardware, UX, and fixture product design (built in close collaboration with Thein & Rios) were developed in-house, in a time frame of only six weeks.</p>
+      <p>My most beautiful nightmare is a website displaying 5 different artworks. Each of those illustrated frames brings the user in a different world with text, sounds illstrations and animations.<br/>
+      This Idea has been developed and powered by my own skills from the illustrations to the developement. I let you discover all the details from the parallax to the font animations.</p>
     </div>
 
     <div class="lil-right">
@@ -30,30 +31,20 @@ Trough this piece of digital poetry, I want to sensibilise people about the beau
       </video>
     </div>
   </div>
-  <div class="separator"></div>
-  <div class="wrapper article">
-    <div class="big-right">
-      <video class="full" loop>
-        <source src="projects/my_most_beautiful_nightmare/img/02.mp4" type="video/mp4">
-      </video>
-      <p>We gave special attention to grant a design quality to the fixtures housing the projector and the flexible fabric.</p>
-    </div>
-    <p class="lil-left"> lol</p>
-  </div>
+
 
 
   <div class="wrapper">
-    <div class="sequence full" speed="20" timeline="false" autoplay=true >
+    <div class="sequence full" speed="17" timeline="false" autoplay=true >
       <div class="separator"></div>
       <div class="full">
-        <img class="full lazyload sequencethumb" src="projects/whitebear/img/white_bear_thumb.jpg" data-src=" "/>
+        <img class="full lazyload sequencethumb" src="projects/my_most_beautiful_nightmare/img/_thumb.jpg" data-src=" "/>
       </div>
-      <img class="full" src="projects/whitebear/img/white_bear_01.jpg" />
-      <img class="full" src="projects/whitebear/img/white_bear_02.jpg" />
-      <img class="full" src="projects/whitebear/img/white_bear_03.jpg" />
-      <img class="full" src="projects/whitebear/img/white_bear_04.jpg" />
-      <img class="full" src="projects/whitebear/img/white_bear_05.jpg" />
-      <img class="full" src="projects/whitebear/img/white_bear_07.jpg" />
+      <img class="full" src="projects/my_most_beautiful_nightmare/img/01.jpg" />
+      <img class="full" src="projects/my_most_beautiful_nightmare/img/02.jpg" />
+      <img class="full" src="projects/my_most_beautiful_nightmare/img/03.jpg" />
+      <img class="full" src="projects/my_most_beautiful_nightmare/img/04.jpg" />
+      <img class="full" src="projects/my_most_beautiful_nightmare/img/05.jpg" />
     </div>
   </div>
 
